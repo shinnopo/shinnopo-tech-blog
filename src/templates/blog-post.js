d43bc721c5a885bc30a98c0be2e3aa4b32843bcb
shinnopo <sin0225skyhigh@icloud.com>
2022-04-16 19:00:47 +0900

    change design

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,6 +5,7 @@ import get from 'lodash/get'
 import Seo from '../components/seo'
 import Layout from '../components/layout'
 import Hero from '../components/hero'
+import Title from '../components/title'
 import Tags from '../components/tags'
 import * as styles from './blog-post.module.css'
 
@@ -23,15 +24,12 @@ class BlogPostTemplate extends React.Component {
         />
         <Hero
           image={post.heroImage?.gatsbyImageData}
-          title={post.title}
-          content={post.description?.childMarkdownRemark?.excerpt}
         />
         <div className={styles.container}>
-          <span className={styles.meta}>
-            {post.author?.name} &middot;{' '}
-            <time dateTime={post.rawDate}>{post.publishDate}</time> –{' '}
-            {post.body?.childMarkdownRemark?.timeToRead} minute read
-          </span>
+          <Title
+            title={post.title}
+            content={post.description?.childMarkdownRemark?.excerpt}
+          />
           <div className={styles.article}>
             <div
               className={styles.body}
