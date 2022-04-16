560ce413642095f26c9cdf71de7773d94c4426ac
shinnopo <sin0225skyhigh@icloud.com>
2022-04-16 18:59:38 +0900

    change nav design

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,21 +6,8 @@ import * as styles from './navigation.module.css'
 const Navigation = () => (
   <nav role="navigation" className={styles.container} aria-label="Main">
     <Link to="/" className={styles.logoLink}>
-      <span className={styles.logo} />
-      <span className={styles.navigationItem}>Gatsby Starter Contentful</span>
+      <span className={styles.navigationItem}>shinnopo tech blog</span>
     </Link>
-    <ul className={styles.navigation}>
-      <li className={styles.navigationItem}>
-        <Link to="/" activeClassName="active">
-          Home
-        </Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/blog/" activeClassName="active">
-          Blog
-        </Link>
-      </li>
-    </ul>
   </nav>
 )
 
